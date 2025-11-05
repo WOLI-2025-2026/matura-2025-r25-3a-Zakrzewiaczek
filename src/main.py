08951f98cbe2193827a2076a9ab4f8dad38c6ac4
Jakub Zakrzewski <93476294+Zakrzewiaczek@users.noreply.github.com>
2025-11-05 17:14:37 +0000

    poprawienie metody 'wszystkie()'

--- src/main.py
+++ src/main.py
@@ -1,23 +1,21 @@
-# Jakub Zakrzewski
+# f = open("zalaczniki-2025/symbole.txt")
+# content = f.read()
 
-f = open("zalaczniki-2025/symbole.txt")
-content = f.read()
+# def zad1():
+#     print([elem for elem in content.split('\n') if (elem[::-1] == elem and elem != '')])
 
-def zad1():
-    print([elem for elem in content.split('\n') if (elem[::-1] == elem and elem != '')])
+# def zad2():
+#     print(f"{str((tablica := [list(elem) for elem in content.split('\n')])[0:0]).replace('[]', '')}{len(wynik := [(y+1, x+1) for y in range(1, len(tablica) - 2) for x in range(1, len(tablica[0]) - 1) if len(set(tablica[b][a] for a, b in [(x-1,y-1),(x,y-1),(x+1,y-1),(x-1,y),(x,y),(x+1,y),(x-1,y+1),(x,y+1),(x+1,y+1)])) == 1])} {' '.join(f'{x} {y}' for x, y in wynik)}")
 
-def zad2():
-    print(f"{str((tablica := [list(elem) for elem in content.split('\n')])[0:0]).replace('[]', '')}{len(wynik := [(y+1, x+1) for y in range(1, len(tablica) - 2) for x in range(1, len(tablica[0]) - 1) if len(set(tablica[b][a] for a, b in [(x-1,y-1),(x,y-1),(x+1,y-1),(x-1,y),(x,y),(x+1,y),(x-1,y+1),(x,y+1),(x+1,y+1)])) == 1])} {' '.join(f'{x} {y}' for x, y in wynik)}")
+# def zad3():
+#     print(f"{(output := max([(int(elem.replace('o','0').replace('+','1').replace('*','2'), 3), elem) for elem in content.split('\n') if elem.strip()], key=lambda x: x[0]))[0]} {output[1]}")
 
-def zad3():
-    print(f"{(output := max([(int(elem.replace('o','0').replace('+','1').replace('*','2'), 3), elem) for elem in content.split('\n') if elem.strip()], key=lambda x: x[0]))[0]} {output[1]}")
-
-def zad4():
-    print(f"{str((to_base3 := lambda n: '' if n == 0 else to_base3(n // 3) + str(n % 3)))[0].replace('<', '')}{(output := sum([int(elem.replace('o','0').replace('+','1').replace('*','2'), 3) for elem in content.split('\n') if elem.strip()]))} {to_base3(output).replace('0','o').replace('1','+').replace('2','*')}")
+# def zad4():
+#     print(f"{str((to_base3 := lambda n: '' if n == 0 else to_base3(n // 3) + str(n % 3)))[0].replace('<', '')}{(output := sum([int(elem.replace('o','0').replace('+','1').replace('*','2'), 3) for elem in content.split('\n') if elem.strip()]))} {to_base3(output).replace('0','o').replace('1','+').replace('2','*')}")
 
 def wszystkie():
-    print(str([elem for elem in content.split('\n') if (elem[::-1] == elem and elem != '')]), f"{str((tablica := [list(elem) for elem in content.split('\n')])[0:0]).replace('[]', '')}{len(wynik := [(y+1, x+1) for y in range(1, len(tablica) - 2) for x in range(1, len(tablica[0]) - 1) if len(set(tablica[b][a] for a, b in [(x-1,y-1),(x,y-1),(x+1,y-1),(x-1,y),(x,y),(x+1,y),(x-1,y+1),(x,y+1),(x+1,y+1)])) == 1])} {' '.join(f'{x} {y}' for x, y in wynik)}", f"{str((to_base3 := lambda n: '' if n == 0 else to_base3(n // 3) + str(n % 3)))[0].replace('<', '')}{(output := sum([int(elem.replace('o','0').replace('+','1').replace('*','2'), 3) for elem in content.split('\n') if elem.strip()]))} {to_base3(output).replace('0','o').replace('1','+').replace('2','*')}", f"{str((to_base3 := lambda n: '' if n == 0 else to_base3(n // 3) + str(n % 3)))[0].replace('<', '')}{(output := sum([int(elem.replace('o','0').replace('+','1').replace('*','2'), 3) for elem in content.split('\n') if elem.strip()]))} {to_base3(output).replace('0','o').replace('1','+').replace('2','*')}", sep = '\n')
-
+    print(str(content := open("zalaczniki-2025/symbole.txt").read())[0].replace('+', '') + str([elem for elem in content.split('\n') if (elem[::-1] == elem and elem != '')]), f"{str((tablica := [list(elem) for elem in content.split('\n')])[0:0]).replace('[]', '')}{len(wynik := [(y+1, x+1) for y in range(1, len(tablica) - 2) for x in range(1, len(tablica[0]) - 1) if len(set(tablica[b][a] for a, b in [(x-1,y-1),(x,y-1),(x+1,y-1),(x-1,y),(x,y),(x+1,y),(x-1,y+1),(x,y+1),(x+1,y+1)])) == 1])} {' '.join(f'{x} {y}' for x, y in wynik)}", f"{str((to_base3 := lambda n: '' if n == 0 else to_base3(n // 3) + str(n % 3)))[0].replace('<', '')}{(output := sum([int(elem.replace('o','0').replace('+','1').replace('*','2'), 3) for elem in content.split('\n') if elem.strip()]))} {to_base3(output).replace('0','o').replace('1','+').replace('2','*')}", f"{str((to_base3 := lambda n: '' if n == 0 else to_base3(n // 3) + str(n % 3)))[0].replace('<', '')}{(output := sum([int(elem.replace('o','0').replace('+','1').replace('*','2'), 3) for elem in content.split('\n') if elem.strip()]))} {to_base3(output).replace('0','o').replace('1','+').replace('2','*')}", sep = '\n')
+    
 # zad1()
 # zad2()
 # zad3()
